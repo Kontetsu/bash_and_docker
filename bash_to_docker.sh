2a0341039e7a02eca9427f2583339b6778198b8a
Erget Kabaj <erget.kabaj@gmail.com>
2021-10-08 23:38:36 +0200

    Complete the script to download and build

--- bash_to_docker.sh
+++ bash_to_docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ####################################################################
 # Made by Erget Kabaj                                              #
 # This is a script to automate Docker and docker compose and build #
@@ -49,10 +48,94 @@ echo "Starting Docker"
 systemctl start docker
 systemctl enable docker
 
-echo "testting if docker is installed correctly"
+echo "Testting if docker is installed correctly"
 
-test_run=docker run hello-world
-echo $test_run
+test=$(systemctl status docker)
+stats=$(echo $?)
 
-if [[ $test_run == $(echo ?)]]
+if [[  $stats == 0 ]]
+then
+    echo "Docker running"
+else
+    echo "Docker not running"
+    exit 1
+fi
 
+echo "Building an docker image "
+
+mkdir $HOME/docker_test && cd $HOME/docker_test
+git clone https://github.com/remy/html5demos.git
+
+cat << EOF > Dockerfile
+
+FROM httpd:latest
+
+COPY ./html5demos/ /usr/local/apache2/htdocs/
+
+EXPOSE 80
+EOF
+
+docker build -t httd_test:v01 -f $HOME/docker_test/
+
+docker run --rm -d -p 80:80 httd_test:v01
+
+echo "Test if the build and the run are succeslfully"
+
+test_run=$(curl localhost:80)
+STATS=$(echo $?)
+
+if [[ $STATS == 0 ]]
+then
+    echo "Build Succesfull"
+    echo "Go to localhost:80 to see the webpage"
+else
+    echo "Not Succesfull check the build"
+    exit 1
+fi
+
+echo "Creating the Composer file"
+
+cat << EOF > docker-compose.yml
+version: "3.7"
+services:
+  httpd:
+    build: httd_test:v01
+    expose: 
+      - 81:80
+
+  nginx:
+    image: nginx:latest
+    volumes: 
+      - /html5demos/www/:/usr/share/nginx/html
+    expose: 
+      - 82:80
+
+volumes:
+  /html5demos/www/
+EOF
+
+docker-compose build && docker-compose up -d
+
+test_run_httpd=$(curl localhost:81)
+STATS=$(echo $?)
+
+if [[ $STATS == 0 ]]
+then
+    echo "Build Succesfull"
+    echo "Go to localhost:81 to see the HTTP webpage build by docker compose"
+else
+    echo "Not Succesfull check the build"
+    exit 1
+fi
+
+test_run_nginx=$(curl localhost:81)
+STATS=$(echo $?)
+
+if [[ $STATS == 0 ]]
+then
+    echo "Build Succesfull"
+    echo "Go to localhost:82 to see the NGINX webpage build by docker compose"
+else
+    echo "Not Succesfull check the build"
+    exit 1
+fi
